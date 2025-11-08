5fc4590bae06be1c76b434782a71ce70980219e8
yu-heejin <jkgt0521@naver.com>
2025-11-08 15:19:32 +0900

    fix: env pydantic 문제 수정

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,8 @@ from pydantic_settings import BaseSettings
 class EnvConfig(BaseSettings):
     # 변수명은 필드명과 같은 이름으로 해야 인식된다.
     db_url: str = Field(env='DB_URL')
+    database: str = Field(env='DATABASE')
+    db_password: str = Field(env='DB_PASSWORD')
 
     class Config:
         env_file = ".env"
