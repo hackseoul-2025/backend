3d8d20ff57ea12ce6dd1ddf5134b6d25b4ac805d
yu-heejin <jkgt0521@naver.com>
2025-11-08 21:46:39 +0900

    chore: statusCode 수정

--- app/api/chat.py
+++ app/api/chat.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 from app.crud.chat import find_chat_messages, insert_chat_message, insert_chat_room, find_chat_rooms
 from app.models.create_chat_message_request import CreateChatMessageRequest
@@ -9,23 +9,23 @@ chat_api = APIRouter(
     tags=["Chat"]
 )
 
-@chat_api.get("/{chat_room_id}/messages")
+@chat_api.get("/{chat_room_id}/messages", status_code=status.HTTP_200_OK)
 def get_chat_messages(chat_room_id: int):
     messages = find_chat_messages(chat_room_id)
     return {"status": 200, "message": messages}
 
-@chat_api.post("/{chat_room_id}/messages")
+@chat_api.post("/{chat_room_id}/messages", status_code=status.HTTP_201_CREATED)
 def create_chat_message(chat_room_id: int, request: CreateChatMessageRequest):
     response = insert_chat_message(chat_room_id=chat_room_id, request=request)
     # TODO: LLM Request and Response
     return {"status": 201, "message": response}
 
-@chat_api.post("/{user_id}")
+@chat_api.post("/{user_id}", status_code=status.HTTP_201_CREATED)
 def create_chat_room(user_id: int, request: CreateChatRoomRequest):
     result = insert_chat_room(user_id, request=request)
     return {"status": 201, "message": {"result": result, "response": f"{request.name}(와)과의 즐거운 대화를 나눠보세요!"}}
 
-@chat_api.get("/{user_id}")
+@chat_api.get("/{user_id}", status_code=status.HTTP_200_OK)
 def get_chat_rooms(user_id: int):
     rooms = find_chat_rooms(user_id)
     return {"status": 200, "message": rooms}
--- app/api/documents.py
+++ app/api/documents.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 from app.crud.s3_bucket import get_rag_documents
 
@@ -7,7 +7,7 @@ document_api = APIRouter(
     tags=["Document"]
 )
 
-@document_api.get("/{category}")
+@document_api.get("/{category}", status_code=status.HTTP_200_OK)
 def get_rag_documents(category: str):
     urls = get_rag_documents(category)
     return {"status": 200, "message": urls}
